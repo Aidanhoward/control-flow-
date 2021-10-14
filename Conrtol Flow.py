20a72c1bc29f45d4f588dd70f2840ebf75aab5af
aidan <aidanhoward757@gmail.com>
2021-10-14 09:53:46 -0700

    created a PIN variable

--- Conrtol Flow.py
+++ Conrtol Flow.py
@@ -14,3 +14,8 @@ first_name = input("What is your first name: ")
 last_name = input("What is your last name: ")
 
 print("\nWelcome to Cash-R-Us",first_name,last_name + ", we will now set up a security pin on your account.\n")
+
+# set up a PIN - Personal Identification Number
+pin = input("please choose a 4 digit Personal Identification Number: ")
+
+print("\nThamk you", first_name + ", we see that you set your PIN to",pin)
