2a562f105f82cff28ddd66cc7433640538e4ecbd
aidan howard <aidanhoward757@gmail.com>
2021-10-21 12:50:53 -0400

    add print statement for ATM

--- Conrtol Flow.py
+++ Conrtol Flow.py
@@ -19,3 +19,5 @@ print("\nWelcome to Cash-R-Us",first_name,last_name + ", we will now set up a se
 pin = input("please choose a 4 digit Personal Identification Number: ")
 
 print("\nThamk you", first_name + ", we see that you set your PIN to",pin)
+
+print("\nWould you like to make a transaction thriugh our atuomated teller machine")
